99aab86605ccf98c81499d6a7cd8665a4d487524
Che Vladimir <cevladimir@mail.ru>
2021-11-09 08:38:44 +1100

    final touch to first task

--- alef.js
+++ alef.js
@@ -7,41 +7,30 @@
 // Описание решения
 // http://mech.math.msu.su
 // 
-let sum = 0;
-const arr = [[399, 9160, 144, 3230, 407, 8875, 1597, 9835], [2093, 3279, 21, 9038, 918, 9238, 2592, 7467], [3531, 1597, 3225, 153, 9970, 2937, 8, 807],
-            [7010, 662, 6005, 4181, 3, 4606, 5, 3980], [6367, 2098, 89, 13, 337, 9196, 9950, 5424], [7204, 9393, 7149, 8, 89, 6765, 8579, 55], 
-            [1597, 4360, 8625, 34, 4409, 8034, 2584, 2], [920, 3172, 2400, 2326, 3413, 4756, 6453, 8], [4914, 21, 4923, 4012, 7960, 2254, 4448, 1]];
 
-for (const row of arr) {
-    outer: for (const el of row) {
-        if (el == 2) {
-            sum += el;
-            console.log("the one --> " + el);
-        } else if (el%2 > 0 && el > 1){            
-            for (let i = 3; i <= Math.sqrt(el); i += 2){
-                // console.log(i);
-                if (el%i==0){
-                    // console.log(el);
-                    continue outer;
-                } 
-                // else if (el%i==0){                    
-                //     // console.log(el + " " + i);
-                //     continue outer;
-                // } 
-                // console.log("2-й -- " + i);
-                // else {
-                //     console.log(el + " " + i + " -- ура!")
-                // }
+const alefVars = {
+    arr : [[399, 9160, 144, 3230, 407, 8875, 1597, 9835], [2093, 3279, 21, 9038, 918, 9238, 2592, 7467], [3531, 1597, 3225, 153, 9970, 2937, 8, 807],
+            [7010, 662, 6005, 4181, 3, 4606, 5, 3980], [6367, 2098, 89, 13, 337, 9196, 9950, 5424], [7204, 9393, 7149, 8, 89, 6765, 8579, 55], 
+            [1597, 4360, 8625, 34, 4409, 8034, 2584, 2], [920, 3172, 2400, 2326, 3413, 4756, 6453, 8], [4914, 21, 4923, 4012, 7960, 2254, 4448, 1]]
+}
+
+function sumOfAllPrimes(arr) {
+    let sum = 0;
+    for (const row of arr) {
+        outer: for (const el of row) {
+            if (el == 2) {
+                sum += el;
+            } else if (el%2 > 0 && el > 1){            
+                for (let i = 3; i <= Math.sqrt(el); i += 2){
+                    if (el%i==0){
+                        continue outer;
+                    } 
+                }
+                sum += el;
             }
-            sum += el;
-            console.log("the one --> " + el);
-            // continue outer;
         }
     }
+    console.log("sum of all primes --> " + sum);
 }
-console.log("sum of all primes --> " + sum);
-// console.log(Math.sqrt(2));
-// console.log(12 % 2);
-// arr.forEach((item, index, array) => {
-//     console.log(`${item} имеет позицию ${index} в ${array}`);
-// });
+
+sumOfAllPrimes(alefVars.arr);
