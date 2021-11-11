f547b2bc1ef687db8f8f5a0ffa1ca3b5ecc8dd33
Che Vladimir <cevladimir@mail.ru>
2021-11-11 13:09:00 +1100

    working on second problem

--- alef_test.php
+++ alef_test.php
@@ -1,15 +1,15 @@
 <?php
-  $arr = array (
-    array(399, 9160, 144, 3230, 407, 8875, 1597, 9835), 
-    array(2093, 3279, 21, 9038, 918, 9238, 2592, 7467), 
-    array(3531, 1597, 3225, 153, 9970, 2937, 8, 807),
-    array(7010, 662, 6005, 4181, 3, 4606, 5, 3980), 
-    array(6367, 2098, 89, 13, 337, 9196, 9950, 5424), 
-    array(7204, 9393, 7149, 8, 89, 6765, 8579, 55), 
-    array(1597, 4360, 8625, 34, 4409, 8034, 2584, 2), 
-    array(920, 3172, 2400, 2326, 3413, 4756, 6453, 8), 
-    array(4914, 21, 4923, 4012, 7960, 2254, 4448, 1)
-  );
+  // $arr = array (
+  //   array(399, 9160, 144, 3230, 407, 8875, 1597, 9835), 
+  //   array(2093, 3279, 21, 9038, 918, 9238, 2592, 7467), 
+  //   array(3531, 1597, 3225, 153, 9970, 2937, 8, 807),
+  //   array(7010, 662, 6005, 4181, 3, 4606, 5, 3980), 
+  //   array(6367, 2098, 89, 13, 337, 9196, 9950, 5424), 
+  //   array(7204, 9393, 7149, 8, 89, 6765, 8579, 55), 
+  //   array(1597, 4360, 8625, 34, 4409, 8034, 2584, 2), 
+  //   array(920, 3172, 2400, 2326, 3413, 4756, 6453, 8), 
+  //   array(4914, 21, 4923, 4012, 7960, 2254, 4448, 1)
+  // );
     
   function sumOfAllPrimes($arr) {
     $sum = 0;
@@ -32,17 +32,37 @@
   }
 
   function randomInteger() {
-    for ($i = 1; $i < 1000; $i++) {
-      $arr[$i] = random_int(1, 1000);
-      // echo "Random number: ".$arr[$i].PHP_EOL;
+    $assocArr = array();
+    $assocArrRepeats = array();
+    for ($i = 1; $i < 10; $i++) {
+      $indexedArr[$i] = random_int(1, 10);
+      echo "Random number: ".$indexedArr[$i].PHP_EOL;
+      if (!array_key_exists($indexedArr[$i], $assocArr)) {
+        $assocArr[$indexedArr[$i]] = $indexedArr[$i];        
+        // echo "Associated Array: ".$assocArr[$indexedArr[$i]].PHP_EOL;
+      } else {
+        if (!array_key_exists($indexedArr[$i], $assocArrRepeats)) {
+          $assocArrRepeats[$indexedArr[$i]] = $indexedArr[$i];
+          // $assocArrSum += $assocArr[$indexedArr[$i]];
+          // echo "Repeats: ".$assocArrRepeats[$indexedArr[$i]].PHP_EOL;          
+        } 
+      }
     }
+    sumOfTheRestInts($assocArr, $assocArrRepeats, $indexedArr);
   }
 
-  function sumOfTheRestInts() {
-    
+  function sumOfTheRestInts($assocArr, $assocArrRepeats, $indexedArr) {    
+    $assocArrSum = 0;
+    for ($i = 0; $i < count($assocArr); $i++) {
+      for ($j = 0; $j < count($assocArrRepeats); $j++) {
+        if (!array_key_exists($assocArrRepeats[$indexedArr[$j]], $assocArr)) {
+          $assocArrSum += $assocArr[$indexedArr[$i]];
+        } 
+      }
+    }    
+    echo "Sum: ".$assocArrSum;
   }
   
   // sumOfAllPrimes($arr);
-  // randomInteger();
-  sumOfTheRestInts();
+  randomInteger();
 ?>
